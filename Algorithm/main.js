d2be552d4a687349f2211c2cfd735d7a949c6920
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-28 16:29:09 +0200

    Basic Node.js file (learning)

---
+++ Algorithm/main.js
@@ -0,0 +1,10 @@
+var http = require('http');
+ 
+var server = http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+});
+ 
+server.listen(3000);
+ 
+console.log('Adresse du serveur: http://localhost:3000');
