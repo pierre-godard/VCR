1a095f1a0488511a8ced591f35e747b54f2f1bd3
julien-monniot <n.ceska@gmail.com>
2015-04-29 09:34:34 +0200

    Google map fragment relocated in VelocityRaptorActivity.. starting to work. Note for later : kill every android StackOverflow members but one.

--- app/src/main/java/fr/insa_lyon/vcr/vcr/VelocityRaptorMain.java
+++ app/src/main/java/fr/insa_lyon/vcr/vcr/VelocityRaptorMain.java
@@ -4,28 +4,34 @@ package fr.insa_lyon.vcr.vcr;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class VelocityRaptorMain extends FragmentActivity implements android.app.FragmentManager.OnBackStackChangedListener {
+
+public class VelocityRaptorMain extends FragmentActivity implements android.app.FragmentManager.OnBackStackChangedListener, OnMapReadyCallback {
 
     // Est-ce qu'on affiche le user input field ou non (sinon on affiche la map).
     private boolean mShowingBack = false;
     private Handler mHandler = new Handler();
-    public static FragmentManager fragmentManager;
+    private GoogleMap mMap;
+    private UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        fragmentManager =  getSupportFragmentManager();
 
         if (savedInstanceState == null) {       // pas d'instance sauvegardée
-            getFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.container, new GoogleMapFragment())     // google map comme fragment principal de la carte
-                    .commit();
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
         } else {
             mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
         }
@@ -74,5 +80,24 @@ public class VelocityRaptorMain extends FragmentActivity implements android.app.
         mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
         // When the back stack changes, invalidate the options menu (action bar).
         invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // position, marker, etc -> setUpMap est ici
+        mMap.setMyLocationEnabled(true);
+        mUiSettings = mMap.getUiSettings();
+
+        // Keep the UI Settings state in sync with the checkboxes.
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setMapToolbarEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setRotateGesturesEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(45.763478, 4.835442), 13));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(45.759948, 4.836593)).title("Lyon").snippet("Snippet Lyon"));
     }
 }
