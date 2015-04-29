e613e284b986916f910779d3b1836e3d1eb816f7
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-29 11:56:33 +0200

    predict update, need intef to database.

--- Algorithm/main.js
+++ Algorithm/main.js
@@ -3,11 +3,11 @@ var http = require('http');
 // Velov station potential state
 var station_state = Object.freeze(
 {
-	EMPTY: "empty", 
+	EMPTY: 		"empty", 
 	NEAR_EMPTY: "near empty", 
-	INTERM: "places available",
-	NEAR_FULL: "near full",
-	FULL: "full"
+	INTERM: 	"places available",
+	NEAR_FULL: 	"near full",
+	FULL: 		"full"
 });
 
 // Mean of the array passed as parameter
@@ -30,9 +30,9 @@ function mean (array)
 // => mean = (1*10+0.5*20+0.25*30)/(1+0.5+0.35)
 function decreasingFactor_mean (factor,step,array) 
 {
-	var sum_val = 0;
-	var final_denominator = 0;
-	var current_factor = 1;
+	var sum_val 			= 0;
+	var final_denominator 	= 0;
+	var current_factor 		= 1;
 	for (i = 0; i < array.length; i++) 
 	{ 
     	sum_val += current_factor*array[i];
@@ -51,26 +51,48 @@ function decreasingFactor_mean (factor,step,array)
 function predict (id,time) 
 {
 	//var week_day = time.getDay();
-	var WEEK_SIZE = 7;
-	var selected_days = [];
-	var limit_date = new Date(2012, 0, 1);
+	var WEEK_SIZE 		= 7;
+	var selected_days 	= [];
+	var station_max		= [];
+	var station_curr	= [];
+	var limit_date 		= new Date(2012, 0, 1);
+	var curr_date		= new Date();
 	for (var d = new Date(time); d > limit_date; d.setDate(d.getDate() - WEEK_SIZE)) 
 	{
-		selected_days.push(new Date(d));
-	}
+		curr_date = new Date(d);
+		selected_days.push(curr_date);
+		station_max.push(getMaxVelov(id,curr_date));
+		station_curr.push(getCurrVelov(id,curr_date));
+	}	
 	return selected_days;
+}
+
+// get the max number of spots in a station (id) at a time
+function getMaxVelov(id,time)
+{
+	var nb = 0;
+	return nb;
+}
+
+// get the current number of spots in a station (id) at a time
+function getCurrVelov(id,time)
+{
+	var nb = 0;
+	return nb;
 }
  
 var server = http.createServer(function(request, response)
 {
-	var mean_arr = [10,20,32];
-	var mean_fac = 0.8;
-	var mean_step = 1;
+	var mean_arr 	= [10,20,32];
+	var mean_fac 	= 0.8;
+	var mean_step 	= 1;
+	var station_id	= 8001;
+	var date 		= Date.now();
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Mean of: "+mean_arr+" = "+mean(mean_arr)+"\n");
     response.write("Decreasing factor mean of: "+mean_arr+" (fac: "+mean_fac+", step: "+mean_step+") = "
     	+decreasingFactor_mean(mean_fac,mean_step,mean_arr)+"\n");
-    response.write("Prediction: "+predict(1,Date.now())+"\n");
+    response.write("Prediction: (station "+station_id+" dates:\n"+predict(8001,date)+"\n");
     response.end('--- END ---\n');
 });
  
