1cb7cbc4f8b2a4a13f89a0ee933b7a5ae0f66aae
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-29 11:11:40 +0200

    Code Jasonification

--- Algorithm/main.js
+++ Algorithm/main.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
 // Velov station potential state
-var station_state = Object.freeze({
+var station_state = Object.freeze(
+{
 	EMPTY: "empty", 
 	NEAR_EMPTY: "near empty", 
 	INTERM: "places available",
@@ -10,9 +11,11 @@ var station_state = Object.freeze({
 });
 
 // Mean of the array passed as parameter
-function mean (array) {
+function mean (array) 
+{
 	var sum_val = 0;
-	for (i = 0; i < array.length; i++) { 
+	for (i = 0; i < array.length; i++) 
+	{ 
     	sum_val += array[i];
 	}
 	return sum_val/i;
@@ -25,14 +28,17 @@ function mean (array) {
 // Step 2: factor = 0.5
 // Step 3: factor = 0.25
 // => mean = (1*10+0.5*20+0.25*30)/(1+0.5+0.35)
-function decreasingFactor_mean (factor,step,array) {
+function decreasingFactor_mean (factor,step,array) 
+{
 	var sum_val = 0;
 	var final_denominator = 0;
 	var current_factor = 1;
-	for (i = 0; i < array.length; i++) { 
+	for (i = 0; i < array.length; i++) 
+	{ 
     	sum_val += current_factor*array[i];
     	final_denominator += current_factor;
-    	if(i%step == 0) {
+    	if(i%step == 0) 
+    	{
     		current_factor *= factor;
     	}    	
 	}
@@ -42,19 +48,21 @@ function decreasingFactor_mean (factor,step,array) {
 // Gives a prediction of the state
 // of a Velov station (id)
 // at a certain point in time (time)
-function predict (id,time) {
+function predict (id,time) 
+{
 	//var week_day = time.getDay();
 	var WEEK_SIZE = 7;
 	var selected_days = [];
 	var limit_date = new Date(2012, 0, 1);
-	for (var d = new Date(time); d > limit_date; d.setDate(d.getDate() - WEEK_SIZE)) {
+	for (var d = new Date(time); d > limit_date; d.setDate(d.getDate() - WEEK_SIZE)) 
+	{
 		selected_days.push(new Date(d));
-		console.log(d);
 	}
 	return selected_days;
 }
  
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response)
+{
 	var mean_arr = [10,20,32];
 	var mean_fac = 0.8;
 	var mean_step = 1;
