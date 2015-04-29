9cc531c907f407e5a651f5d19b2951bf9f1a1036
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-29 09:30:37 +0200

    Updated basic main with mean function

--- Algorithm/main.js
+++ Algorithm/main.js
@@ -1,7 +1,21 @@
 var http = require('http');
+
+function mean (array) {
+	var mean_val = 0;
+	for (i = 0; i < array.length; i++) { 
+    	mean_val += array[i];
+	}
+	return mean_val/i;
+}
+
+function predict () {
+
+}
  
 var server = http.createServer(function(request, response){
+	var arr = [10,20,30];
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Mean of: "+arr+" = "+mean(arr)+"\n");
     response.end('Hello World\n');
 });
  
