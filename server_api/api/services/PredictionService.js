5fbc3f749f0479a0e3a1da2134a935f213c2a3c9
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-29 17:22:08 +0200

    Prediction sercice code updated to match service architecture

--- server_api/api/services/PredictionService.js
+++ server_api/api/services/PredictionService.js
@@ -2,7 +2,7 @@
 //	main.js
 //	Prediction
 //	Description:
-//		This module predicts 
+//		This service predicts the state of a station
 //	Created: 	28/04/2015
 //	By: 		Samuel MAGNAN
 // ==============================================================
@@ -73,56 +73,6 @@ function decreasingFactor_mean (factor,step,array)
 	return sum_val/final_denominator;
 }
 
-// Gives a prediction of the state
-// of a Velov station (id)
-// at a certain point in time (time) 
-// using the selected analysis algorithm (analysisMode) 
-function predict (id,time,analysisMode) 
-{
-	// ----- Data fetching
-	var WEEK_SIZE 		= 7;
-	var selected_days 	= [];
-	var station_max		= [];
-	var station_curr	= [];
-	var limit_date 		= new Date(2012, 0, 1);
-	var curr_date		= new Date();
-	for (var d = new Date(time); d > limit_date; d.setDate(d.getDate() - WEEK_SIZE)) 
-	{
-		curr_date = new Date(d);
-		selected_days.push(curr_date);
-		station_max.push(getMaxVelov(id,curr_date));
-		station_curr.push(getCurrVelov(id,curr_date));
-	}
-
-	// ----- Data analysis
-	var max_overTime 	= analysis(station_max,analysisMode);
-	var curr_overTime 	= analysis(station_curr,analysisMode);
-	var diff_overTime 	= max_overTime - curr_overTime;
-
-	console.log("max_overTime:  "+max_overTime);
-	console.log("curr_overTime: "+curr_overTime);
-	console.log("diff_overTime: "+diff_overTime);
-
-	// ----- State selection
-	if (diff_overTime < station_values.FULL_LIMIT)
-	{
-		return station_state.FULL;
-	} 
-	else if (diff_overTime <= station_values.NEAR_FULL_LIMIT)
-	{
-		return station_state.NEAR_FULL;
-	}
-	else if (curr_overTime < station_values.EMPTY_LIMIT)
-	{
-		return station_state.EMPTY;
-	}
-	else if (curr_overTime <= station_values.NEAR_EMPTY_LIMIT)
-	{
-		return station_state.NEAR_EMPTY;
-	}
-	return station_state.INTERM;
-}
-
 // Analyse a set of datas using the selected mode
 // Returns the caracteristic value associated with the analysis
 function analysis (datas,mode) 
@@ -154,23 +104,55 @@ function getCurrVelov(id,time)
 	var nb = 0;
 	return nb;
 }
- 
-var server = http.createServer(function(request, response)
-{
-	var mean_arr 		= [10,20,32];
-	var mean_fac 		= 0.8;
-	var mean_step 		= 1;
-	var station_id		= 8001;
-	var date 			= Date.now();
-	var analysisMode 	= analysis_mode.MEAN;
-    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    response.write("Mean of: "+mean_arr+" = "+mean(mean_arr)+"\n");
-    response.write("Decreasing factor mean of: "+mean_arr+" (factor: "+mean_fac+", step: "+mean_step+") = "
-    	+decreasingFactor_mean(mean_fac,mean_step,mean_arr)+"\n");
-    response.write("Prediction using "+analysisMode+" (date: "+new Date(date)+" - station: "+station_id+"): "+predict(8001,date,analysisMode)+"\n");
-    response.end('--- END ---\n');
-});
- 
-server.listen(3000);
- 
-console.log('Adresse du serveur: http://localhost:3000');
+
+module.exports = {
+	// Gives a prediction of the state
+	// of a Velov station (id)
+	// at a certain point in time (time) 
+	// using the selected analysis algorithm (analysisMode) 
+	predict: function (id,time,analysisMode,callback) 
+	{
+		// ----- Data fetching
+		var WEEK_SIZE 		= 7;
+		var selected_days 	= [];
+		var station_max		= [];
+		var station_curr	= [];
+		var limit_date 		= new Date(2012, 0, 1);
+		var curr_date		= new Date();
+		for (var d = new Date(time); d > limit_date; d.setDate(d.getDate() - WEEK_SIZE)) 
+		{
+			curr_date = new Date(d);
+			selected_days.push(curr_date);
+			station_max.push(getMaxVelov(id,curr_date));
+			station_curr.push(getCurrVelov(id,curr_date));
+		}
+
+		// ----- Data analysis
+		var max_overTime 	= analysis(station_max,analysisMode);
+		var curr_overTime 	= analysis(station_curr,analysisMode);
+		var diff_overTime 	= max_overTime - curr_overTime;
+
+		console.log("max_overTime:  "+max_overTime);
+		console.log("curr_overTime: "+curr_overTime);
+		console.log("diff_overTime: "+diff_overTime);
+
+		// ----- State selection
+		if (diff_overTime < station_values.FULL_LIMIT)
+		{
+			callback(station_state.FULL);
+		} 
+		else if (diff_overTime <= station_values.NEAR_FULL_LIMIT)
+		{
+			callback(station_state.NEAR_FULL);
+		}
+		else if (curr_overTime < station_values.EMPTY_LIMIT)
+		{
+			callback(station_state.EMPTY);
+		}
+		else if (curr_overTime <= station_values.NEAR_EMPTY_LIMIT)
+		{
+			callback(station_state.NEAR_EMPTY);
+		}
+		callback(station_state.INTERM);
+	}
+};
