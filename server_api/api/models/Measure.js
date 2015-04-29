28f3e93225f69c758faaadda9e98e71fb43abae3
Samuel MAGNAN <sam.samuel.magnan@gmail.com>
2015-04-29 23:21:29 +0200

    Mesure contains new datas (time based) used for learning / analysis

--- server_api/api/models/Measure.js
+++ server_api/api/models/Measure.js
@@ -7,11 +7,25 @@
 
 module.exports = {
 
+    NB_TIME_SLICES: 12,
+
     attributes: {
         last_update: {
             type: 'integer',
             required: true,
             primaryKey: true
+        },
+        day: {
+            type: 'string',
+            required: true
+        },
+        hour: {
+            type: 'integer',
+            required: true
+        },
+        time_slice: {
+            type: 'integer',
+            required: true
         },
         station: {
             model: 'Station',
@@ -35,6 +49,11 @@ module.exports = {
             value.latitude = value.position.lat;
         }
         value.station = value.number;
+        var d = new Date(0);    // The 0 there is the key, which sets the date to the epoch
+        d.setUTCSeconds(value.last_update);
+        value.day = d.getDay();
+        value.hour = d.getHours(); 
+        value.time_slice = Math.floor(d.getMinutes()/Measure.NB_TIME_SLICES);
         delete value['number'];
         delete value['name'];
         delete value['address'];
