7be68fadbac357c91fe254ae53f1e26b2d39f57b
julien-monniot <n.ceska@gmail.com>
2015-04-29 15:32:40 +0200

    Starting to implement some network utilities

---
+++ app/src/main/java/fr/insa_lyon/vcr/vcr/ServerConnection.java
@@ -0,0 +1,86 @@
+package fr.insa_lyon.vcr.vcr;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * Created by julien on 29/04/15.
+ */
+public class ServerConnection {
+
+    // Response from the HTTP Request
+    static InputStream httpResponseStream = null;
+    // JSON Response String to create JSON Object
+    static String jsonString = "";
+
+    // Method to issue HTTP request, parse JSON result and return JSON Object
+    public JSONObject makeHttpGet(String url, List<NameValuePair> params) {
+        try {
+            // client http default
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            // Format the parameters correctly for HTTP transmission
+            String paramString = URLEncodedUtils.format(params, "utf-8");
+            // Add parameters to url in GET format
+            url += "?" + paramString;
+            // Execute the request
+            HttpGet httpGet = new HttpGet(url);
+            // Execute the request and fetch Http response
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            // Extract the result from the response
+            HttpEntity httpEntity = httpResponse.getEntity();
+            // Open the result as an input stream for parsing
+            httpResponseStream = httpEntity.getContent();
+            // Catch Possible Exceptions
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // buffered reader httpResponseStream
+            BufferedReader httpResponseReader = new BufferedReader(
+                    new InputStreamReader(httpResponseStream, "iso-8859-1"), 8);
+            // String to hold current line from httpResponseReader
+            String line = null;
+            // Clear jsonString
+            jsonString = "";
+            // While there is still more response to read
+            while ((line = httpResponseReader.readLine()) != null) {
+                // Add line to jsonString
+                jsonString += (line + "\n");
+            }
+            // Close Response Stream
+            httpResponseStream.close();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        try {
+            // Create jsonObject from the jsonString and return it
+            return new JSONObject(jsonString);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+            // Return null if in error
+            return null;
+        }
+    }
+}
--- app/src/main/java/fr/insa_lyon/vcr/vcr/VelocityRaptorMain.java
+++ app/src/main/java/fr/insa_lyon/vcr/vcr/VelocityRaptorMain.java
@@ -10,16 +10,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class VelocityRaptorMain extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private UiSettings mUiSettings;
-    private CameraPosition saveCamera;
+    private ServerConnection serverConnect;
+    String serverUrl;
+    List<NameValuePair> requestParams;
+    JSONObject infoStationsJSON;
+    boolean serverInitOk = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,24 @@ public class VelocityRaptorMain extends FragmentActivity implements OnMapReadyCa
                     (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
         }
-
+        if (!(serverInitOk = serverInit())) {
+            finish();
+        }
     }
+
+    private boolean serverInit() {
+        if (!serverInitOk) {
+            serverUrl = "";
+            requestParams.add(new BasicNameValuePair("toto", "titi"));
+            serverConnect = new ServerConnection();
+            infoStationsJSON = serverConnect.makeHttpGet(serverUrl, requestParams);
+            if (infoStationsJSON != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public void onButtonClickedMain(View v) {
         if (v == findViewById(R.id.but_recherche)) {
