ff25e102f1aa74353703152a8670007f4fe7254e
julien-monniot <n.ceska@gmail.com>
2015-04-29 14:27:05 +0200

    UserInput switched from ActionBarActivity to Activity. Because.

--- app/src/main/java/fr/insa_lyon/vcr/vcr/UserInput.java
+++ app/src/main/java/fr/insa_lyon/vcr/vcr/UserInput.java
@@ -1,12 +1,10 @@
 package fr.insa_lyon.vcr.vcr;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class UserInput extends ActionBarActivity {
+public class UserInput extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,19 +12,4 @@ public class UserInput extends ActionBarActivity {
         setContentView(R.layout.activity_user_input);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user_input, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return super.onOptionsItemSelected(item);
-    }
 }
